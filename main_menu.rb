805ae7e15fd0ce7e4f077ca818606068963f0fe2
Trevor Watson <watsontrevor10@gmail.com>
2019-11-08 12:56:42 -0700

    got the main menu up and working, all files referenced

--- main_menu.rb
+++ main_menu.rb
@@ -1,15 +1,25 @@
+require_relative 'black_jack'
+require_relative 'coin_toss'
+require_relative 'horse_race'
+require_relative 'roulette'
+require_relative 'rowshambow'
+
 class Player
+attr_accessor :name
 
-  def players
-
+  def initialize
+    # main variables
+    @player_name = nil
   end
+
 end
 
 
 class Wallet
 
-  def wallets
-
+  def initialize
+    # main wallet variable
+    @player_wallet = 50
   end
 
 end
@@ -17,4 +27,58 @@ end
 
 class Menu
 
+  def initialize
+    @player = Player.new 
+  end
+
+  def start
+    puts "What is your name"
+
+    @player_name = gets.strip
+    puts ""
+    puts "Welcome #{@player_name}"
+
+    main_menu
+  end
+
+  def main_menu
+    puts "-- Welcome to the Casino --"
+    puts ""
+    puts "- Choose what you want to do -"
+    puts ""
+    puts "RowShamBow"
+    puts "Coin Toss"
+    puts "Roulette"
+    puts "Horse Racing"
+    puts "Quit"
+    puts ""
+  
+    @game_choice = gets.strip.downcase
+
+    case @game_choice
+    when "rowshambow", "roshambo", "roshambow", "rowshambo"
+      # open rowshambow app
+      load 'rowshambow.rb'
+    when "coin toss", "cointoss"
+      # open coin toss app
+      load 'coin_toss.rb'
+    when "roulette"
+      # open roulette app
+      load 'roulette.rb'
+    when "horse racing", "horse race", "race"
+      # open horse racing app
+      load 'horse_race.rb'
+    when "quit", "q", "exit"d
+      # exits app
+      puts "Thank you for playing!"
+    else 
+      puts "Invalid choice. Try again."
+      main_menu
+    end
+  end
+
 end
+
+Wallet.new
+menu_init = Menu.new
+menu_init.start
