eb927d83000f8e98d8c995a9bb2a0d1098f419e9
Jacob Bradford <jcbbrdfrd@yahoo.com>
2019-11-08 15:14:56 -0700

    updated the horse race class

--- horse_race.rb
+++ horse_race.rb
@@ -1,10 +1,10 @@
 # Jacob
-requrie_relative "main_menu"
+require_relative "main_menu"
 
 class HorseRace
 
-  def initialize
-    @player 
+  def initialize(player)
+    @player = player
     @horses = ["Captain", "Lucky", "Last Chance", "Admiral"]
 
     puts "Welcome to the Racetrack!"
@@ -15,8 +15,24 @@ class HorseRace
       puts "#{index}. #{horse}"
     end
 
+    print "Selection: >"
+    player_horse = gets.strip
+
+    print "Place a bet: >"
+    bet = gets.to_i
+
+    winning_horse = @horses.sample
+    "#{winning_horse} wins!!!"
+
+    case winning_horse
+      when player_horse
+         "You are a winner!"
+      else
+        "You lost better luck next time!" 
+    end 
+
   end
 
 end
 
-HorseRace.new
+HorseRace.new()
