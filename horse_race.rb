0e57d918ae0429a415207a89f3cc818607bbaa3e
Jacob Bradford <jcbbrdfrd@yahoo.com>
2019-11-08 14:55:26 -0700

    added the horse race class

--- horse_race.rb
+++ horse_race.rb
@@ -1,1 +1,22 @@
 # Jacob
+requrie_relative "main_menu"
+
+class HorseRace
+
+  def initialize
+    @player 
+    @horses = ["Captain", "Lucky", "Last Chance", "Admiral"]
+
+    puts "Welcome to the Racetrack!"
+    puts "Plese select a horse."
+    puts "--- Horses ---"
+
+    @horses.each_with_index do |horse, index|
+      puts "#{index}. #{horse}"
+    end
+
+  end
+
+end
+
+HorseRace.new
